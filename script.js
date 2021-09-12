80a02f31ab7145332d3ef5cfd0db2ae264ada05c
David Olea <david@Davids-MacBook-Air.local>
2021-09-12 00:06:50 -0700

    test1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var buttonFunction = function(){
     let a = location.href = 'https://accounts.spotify.com/authorize?client_id=8e0821c9695d4b1aa5c76936afe8cbe6&redirect_uri=https://davido1214.github.io/test/&response_type=token'
      
-    var myURl = function(){
-        console.log("hi")
-        console.log(window.location.href)
-    }
-   setInterval(myURl(),5000)
+
+   setInterval(function(){
+    console.log("hi")
+    console.log(window.location.href)
+   },3000)
     
     }
      
