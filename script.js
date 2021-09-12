ecd74310b6163c59270a1f89c1d7aa993b3d2e72
David Olea <david@Davids-MacBook-Air.local>
2021-09-12 02:57:14 -0700

    test1

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-var buttonFunction = function(){
-    location.href = 'https://accounts.spotify.com/authorize?client_id=8e0821c9695d4b1aa5c76936afe8cbe6&redirect_uri=https://davido1214.github.io/test/&response_type=token'
-    (setTimeout(function(){
-        var savedUrl = window.location.href
-        var cut = savedUrl.split("=")
-        var cut2 = cut[1]
-        var cut3 = cut2.split("&")
-        var cut4 = cut3[0]
-        var cut5 = cut4.split(",")
-        var cut6 = cut5[0]
-        console.log(cut6)
-        getCode(cut6)
-        
-
-       },1000))
-    }
-     
     var getApis = function() {
 
-        var getCode = function(theCode){
-            var x = theCode
-        }
-        console.log(getCode(theCode))
+        document.getElementById("btn").addEventListener("click",function(){
+            location.href = 'https://accounts.spotify.com/authorize?client_id=8e0821c9695d4b1aa5c76936afe8cbe6&redirect_uri=https://davido1214.github.io/test/&response_type=token'
+        (setTimeout(function(){
+            var savedUrl = window.location.href
+            var cut = savedUrl.split("=")
+            var cut2 = cut[1]
+            var cut3 = cut2.split("&")
+            var cut4 = cut3[0]
+            var cut5 = cut4.split(",")
+            var cut6 = cut5[0]
+            console.log(cut6)
+
+            fetch("https://api.spotify.com/v1/search?q=" + /*this will be the mood selector -> */"Sad" +
+     
+       "&type=" + /* this will be where the music options are selected -> */ "track%2Cartist%2Cplaylist"
+     
+        + "&market=US&limit=5&offset=5", {
+           headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json',
+               // the var AccesToken would replace the random string that make up the old token
+               'Authorization': 'Bearer'+ cut6,
+              
+            }
+       })
+           .then(function(response) {
+               return response.json();
+           })
+           .then(function(data){
+               console.log(data);
+           });
+
+         },1000))
+
+        })
+        
         
        //this link is used to redirect the user to login. Right now the link redirects to one of my gitpages but once this page is properly running we switch the link to this page. Once redirected and authorized the url generates a new auth link which we need to replace the old expired one. (link expires every hour)
      
@@ -36,26 +51,6 @@ var buttonFunction = function(){
     //        console.log(data)
     //    })
      
-
-       fetch("https://api.spotify.com/v1/search?q=" + /*this will be the mood selector -> */"Sad" +
-     
-       "&type=" + /* this will be where the music options are selected -> */ "track%2Cartist%2Cplaylist"
-     
-        + "&market=US&limit=5&offset=5", {
-           headers: {
-               'Accept': 'application/json',
-               'Content-Type': 'application/json',
-               // the var AccesToken would replace the random string that make up the old token
-               'Authorization': 'Bearer'+ getCode(theCode),
-              
-            }
-       })
-           .then(function(response) {
-               return response.json();
-           })
-           .then(function(data){
-               console.log(data);
-           });
      
        var qouteApi = "https://goquotes-api.herokuapp.com/api/v1/all/quotes?New%20item=" + /* this will be the mood selector for quote api -> */ "sad";
      
