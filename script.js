76b0bba25f520561e56d320a0551d3c6c3e06dbf
David Olea <david@Davids-MacBook-Air.local>
2021-09-12 01:21:35 -0700

    test1

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ var buttonFunction = function(){
         var cut2 = cut[1]
         var cut3 = cut2.split("&")
         userToken = cut3
-        console.log(userToken+"wrong function")
-        return userToken
+        console.log(cut3+"wrong function")
        },3000))
      
 
@@ -16,7 +15,6 @@ var buttonFunction = function(){
     }
      
     var getApis = function(search) {
-        console.log(userToken+"")
         
        //this link is used to redirect the user to login. Right now the link redirects to one of my gitpages but once this page is properly running we switch the link to this page. Once redirected and authorized the url generates a new auth link which we need to replace the old expired one. (link expires every hour)
      
